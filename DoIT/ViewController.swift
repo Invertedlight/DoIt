0953608f4a1528ba0ce5b37a27cb450f95439ccf
James Richardson <james.richardson@invertedlight.com>
2020-05-10 12:29:25 -0700

    Initial Commit

---
+++ DoIT/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DoIT
+//
+//  Created by James Richardson on 5/10/20.
+//  Copyright © 2020 James Richardson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
