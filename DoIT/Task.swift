e1c906b25de4da51ebe2a5f9a8a434f31952e06a
James Richardson <james.richardson@invertedlight.com>
2020-05-10 14:24:18 -0700

    Added  viewController and Cell generation based on number o tasks created.

---
+++ DoIT/Task.swift
@@ -0,0 +1,14 @@
+//
+//  Task.swift
+//  DoIT
+//
+//  Created by James Richardson on 5/10/20.
+//  Copyright © 2020 James Richardson. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    var name = ""
+    var important = false
+}
--- DoIT/ViewController.swift
+++ DoIT/ViewController.swift
@@ -8,13 +8,52 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        let task = tasks[indexPath.row]
+        if task.important {
+            cell.textLabel?.text = "❗️ \(task.name)"
+        } else {
+            cell.textLabel?.text = task.name
+        }
+        
+        return cell
+    }
+    
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var tasks : [Task] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        tasks = makeTasks()
+        
+        tableView.dataSource = self
+        tableView.delegate = self
     }
 
+    func makeTasks() -> [Task] {
+        let task1 = Task()
+        let task2 = Task()
+        let task3 = Task()
+        task1.name = "Walk the dog"
+        task1.important = false
+        task2.name = "Buy cheese"
+        task2.important = true
+        task3.name = "Mow lawn"
+        task3.important = false
+        
+        return [task1, task2, task3]
+    }
 
 }
 
